de23246593dda27ad454069d9b4ef9c770b90dcd
joams mogro gomez <joamsmg@gmail.com>
2025-10-27 00:37:17 -0400

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,57 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import winston from 'winston';
+import { WinstonModule, utilities } from 'nest-winston';
+
+const logger = WinstonModule.createLogger({
+  levels: winston.config.npm.levels,
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.simple(),
+        utilities.format.nestLike('BookStore', {
+          prettyPrint: true,
+        }),
+      ),
+    }),
+    new winston.transports.File({
+      filename: 'logs/app.log',
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+    new winston.transports.File({
+      filename: 'logs/app.log',
+      level: 'error',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+  ],
+});
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger });
+  app.enableCors();
+  app.use(cookieParser());
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(
+    'info',
+    `🟢 Server running on port ${process.env.PORT ?? 3000} 🟢`,
+  );
 }
-bootstrap();
+void bootstrap();
