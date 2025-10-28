d271b670a0355c017c2aaf9fbf65c8bae14c18d8
joams mogro gomez <joamsmg@gmail.com>
2025-10-28 11:37:58 -0400

    feat: some const were moved to new files

--- src/main.ts
+++ src/main.ts
@@ -2,48 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
-import winston from 'winston';
-import { WinstonModule, utilities } from 'nest-winston';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-const logger = WinstonModule.createLogger({
-  levels: winston.config.npm.levels,
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.colorize(),
-        winston.format.simple(),
-        utilities.format.nestLike('BookStore', {
-          prettyPrint: true,
-        }),
-      ),
-    }),
-    new winston.transports.File({
-      filename: 'logs/app.log',
-      level: 'info',
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
-    }),
-    new winston.transports.File({
-      filename: 'logs/app.log',
-      level: 'error',
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
-    }),
-  ],
-});
-
-const config = new DocumentBuilder()
-  .setTitle('Book Store')
-  .setDescription('The Book Store API')
-  .setVersion('1.0')
-  .addTag('books')
-  .build();
+import { SwaggerModule } from '@nestjs/swagger';
+import { loggerConfig as logger } from './config/logger.config';
+import { swaggerConfig as config } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger });
