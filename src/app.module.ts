51094db82d7cbe667526b8cc534b195a150dd65b
joams mogro gomez <joamsmg@gmail.com>
2025-10-31 01:19:22 -0400

    feat: prisma module was added globaly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './config/prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
